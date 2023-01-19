86b630750042e83062f17e848291b21d6beba7cf
maxkallio <maxkallio7@gmail.com>
2023-01-20 00:48:10 +0200

    ss

--- 3.py
+++ 3.py
@@ -1,5 +1,6 @@
 min_number = 0
 max_number = 0
+first_number = False
 
 while True:
     try:
@@ -9,12 +10,18 @@ while True:
 
         else:
             number = int(number)
+            if first_number == False:
+                min_number = number
+                max_number = number
+                first_number = True
+
             if max_number > number > min_number:
                 pass
-            if number > max_number:
+
+            elif number > max_number:
                 max_number = number
 
-            if number < min_number:
+            elif number < min_number:
                 min_number = number
 
             continue
